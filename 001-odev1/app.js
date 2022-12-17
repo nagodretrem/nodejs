d6753d951430193ebb1f96b73e22f16502e2d39b
Mert <nagodretrem@gmail.com>
2022-12-17 15:56:41 +0300

    Circle are calculation with console input

---
+++ 001-odev1/app.js
@@ -0,0 +1,11 @@
+//Circle area calculation with console input
+
+const arguments = process.argv.slice(2);
+const radius = arguments[0]*1;
+const pi = 3.14;
+const area = pi * radius * radius;
+console.log(area);
+
+
+
+//Path: 001-odev1\app.js
