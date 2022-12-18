ca640e68c06ecb22c4e0abd7054de8246c777622
Mert <nagodretrem@gmail.com>
2022-12-18 16:42:30 +0300

    Module concept

---
+++ 003-circleModule/circleFunction.js
@@ -0,0 +1,15 @@
+const circleArea = (radius) => {
+    return Math.PI * radius * radius;
+}
+
+const circleCircumference = (radius) => {
+    return 2 * Math.PI * radius;
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
+
+//ES6 export using
+//export { circleArea, circleCircumference}
---
+++ 003-circleModule/index.js
@@ -0,0 +1,8 @@
+const { circleArea, circleCircumference } = require('./circleFunction');
+
+console.log("Circle radius: 5");
+console.log("Circle area: ",circleArea(5));
+console.log("Circle circumference: ",circleCircumference(5));
+
+//ES6 import using
+//import { circleArea, circleCircumference } from './circleFunction';
